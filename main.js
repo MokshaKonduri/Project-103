55c8dc70958acf5fa09b453a810f3229eb9491ba
Moksha Konduri <73599148+MokshaKonduri@users.noreply.github.com>
2021-03-23 21:14:13 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,17 +1,32 @@
-function preload() {
-
+Webcam.set({
+width:350,
+height:300,
+image_format:'png',
+png_quality:90
+});
+camera=document.getElementById("camera");
+Webcam.attach('#camera');
+function take_snapshot(){
+    Webcam.snap(function(data_uri){
+document.getElementById("result").innerHTML= document.getElementById("result").innerHTML='<img id="captured_image" src="'  +  data_uri +'"/ >';
+    });
 }
-function setup() {
-    canvas=createCanvas(640,480);
-    canvas.position(110,250);
-    myVideo=createCapture(VIDEO);
-    myVideo.hide();
-    tint_color="";
+console.log('ml5 version:',ml5.version);
+classifier=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/VvFI2oCbM/model.json',modelLoaded);
+function modelLoaded() {
+    console.log("model is Loaded")
 }
-function draw() {
-    image(myVideo,0,0,640,480);
-    tint(tint_color);
+function check() {
+    img=document.getElementById('captured_image');
+    classifier.classify(img,gotResult);
 }
-function take_snapshot() {
-    save('screenshot.png');
+function gotResult(error,results) {
+    if (error) {
+        console.error(error);
+    }
+    else {
+        console.log(results);
+        document.getElementById("result_object_name").innerHTML=results[0].label;
+        document.getElementById("result_object_accuracy").innerHTML=results[0].confidence.toFixed(3);
+    }
 }
